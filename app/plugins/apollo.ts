bd7af9fabc24df2d31887b5c214f946e605b6695
Larry W <l422y@l422y.com>
2023-02-17 01:02:59 -0500

    adjust timing

--- app/plugins/apollo.ts
+++ app/plugins/apollo.ts
@@ -1,7 +1,8 @@
-import {useUserStore} from "~/store/user"
-import {parseJwt} from "~/composables/useJwtUtils"
+import { useUserStore } from "~/store/user"
+import { parseJwt } from "~/composables/useJwtUtils"
+// @ts-ignore
 import refreshToken from "~/gql/refreshJwtAuthToken.gql"
-import {_AsyncData} from "#app/composables/asyncData"
+import { _AsyncData } from "#app/composables/asyncData"
 
 export default defineNuxtPlugin((nuxtApp) => {
   let refreshingToken = false
@@ -10,10 +11,9 @@ export default defineNuxtPlugin((nuxtApp) => {
   const tryRefresh = async () => {
     if (!refreshingToken && userStore?.refreshToken) {
       const expiryDate = new Date(parseJwt(userStore.refreshToken).exp * 1000)
-      const beforeExpiry = new Date(expiryDate.getTime() - 84000 * 1000)
+      const beforeExpiry = new Date(expiryDate.getTime() - (86000 * 1000))
       const shouldRenew = new Date().getTime() > beforeExpiry.getTime()
       if (shouldRenew) {
-        console.log("expired! renewing...")
         refreshingToken = true
         await useAsyncQuery<any>(refreshToken, {jwtRefreshToken: userStore.refreshToken})
           .then((result: _AsyncData<any, Error>) => {
